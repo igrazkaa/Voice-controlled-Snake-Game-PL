e0455339410f0bb3e5a3f847f0d596aabdc88bf8
igrazkaa <103316739+igrazkaa@users.noreply.github.com>
2022-11-19 12:43:26 +0100

    Adding files

---
+++ asr_whisper.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+from pathlib import Path
+
+
+def asr_whisper(asr_model):
+    model = asr_model
+
+    try:
+        r = sr.Recognizer()
+
+        with sr.Microphone() as source:
+            print('Recording...')
+            audio = r.record(source, duration=1.5)
+            with open("recording.wav", "wb+") as f:
+                f.write(audio.get_wav_data())
+        print('Transcribing...')
+        path = Path('recording.wav').absolute()
+        result = model.transcribe('recording.wav', fp16=False, language="pl", initial_prompt='góra dół lewo prawo')
+        text = result['text'].lower()
+        print(text)
+        return text
+    except sr.UnknownValueError:
+        return ''
